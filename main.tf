9e61cf88a346fb6ad66902dd9d1e254039fc0c3a
Liam F. O'Neill <liamfoneill@users.noreply.github.com>
2018-01-08 16:42:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv" {
   name                = "${var.product}-${var.vault_name_suffix}"
   location            = "${var.location}"
-  resource_group_name = "${var.product}-${var.resource_group_name_suffix}"
+  resource_group_name = "${var.product}-${var.env}"
 
   sku {
     name = "standard"
