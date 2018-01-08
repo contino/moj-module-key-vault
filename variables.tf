033d800d86a09db836e09d860669ae134b25148a
Liam F. O'Neill <liamfoneill@users.noreply.github.com>
2018-01-08 14:14:34 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "tenant_id" {
     description                 = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault."
 }
 variable "object_id" {
-    type = "string"
+    type                        = "string"
     description                 = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
