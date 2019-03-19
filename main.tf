fa1bcd2867b2727c9628a34b42572a06b703c49c
Tim Jacomb <tim.jacomb@hmcts.net>
2019-03-19 15:44:46 +0000

    Use access policies

--- main.tf
+++ main.tf
@@ -3,95 +3,94 @@ locals {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name = "${local.vaultName}"
-  location = "${var.location}"
+  name                = "${local.vaultName}"
+  location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 
   sku {
-    name = "standard"
+    name = "${var.sku}"
   }
 
   tenant_id = "${var.tenant_id}"
 
-  access_policy {
-    tenant_id = "${var.tenant_id}"
-    object_id = "${var.object_id}"
-
-    certificate_permissions = [
-      "create",
-      "delete",
-      "deleteissuers",
-      "get",
-      "getissuers",
-      "import",
-      "list",
-      "listissuers",
-      "setissuers",
-      "update",
-      "managecontacts",
-      "manageissuers",
-    ]
-
-    key_permissions = [
-      "create",
-      "list",
-      "get",
-      "delete",
-      "update",
-      "import",
-      "backup",
-      "restore",
-    ]
-
-    secret_permissions = [
-      "set",
-      "list",
-      "get",
-      "delete",
-    ]
-  }
-
-  access_policy {
-    object_id = "${var.product_group_object_id}"
-    tenant_id = "${var.tenant_id}"
-
-    key_permissions = [
-      "list",
-      "update",
-      "create",
-      "import",
-      "delete"
-    ]
-    certificate_permissions = [
-      "list",
-      "update",
-      "create",
-      "import",
-      "delete",
-      "managecontacts",
-      "manageissuers",
-      "getissuers",
-      "listissuers",
-      "setissuers",
-      "deleteissuers"
-    ]
-    secret_permissions = [
-      "list",
-      "set",
-      "delete"
-    ]
-  }
-
-  enabled_for_disk_encryption = true
-  enabled_for_deployment = true
+  enabled_for_disk_encryption     = true
+  enabled_for_deployment          = true
   enabled_for_template_deployment = true
 
-  tags = "${merge(var.common_tags,
-    map(
-      "environment", var.env,
-      "product", var.product
-    )
-  )}"
-
+  tags = "${var.common_tags}"
 }
 
+resource "azurerm_key_vault_access_policy" "creator_access_policy" {
+  key_vault_id = "${azurerm_key_vault.kv.id}"
+
+  object_id = "${var.object_id}"
+  tenant_id = "${var.tenant_id}"
+
+  certificate_permissions = [
+    "create",
+    "delete",
+    "deleteissuers",
+    "get",
+    "getissuers",
+    "import",
+    "list",
+    "listissuers",
+    "setissuers",
+    "update",
+    "managecontacts",
+    "manageissuers",
+  ]
+
+  key_permissions = [
+    "create",
+    "list",
+    "get",
+    "delete",
+    "update",
+    "import",
+    "backup",
+    "restore",
+  ]
+
+  secret_permissions = [
+    "set",
+    "list",
+    "get",
+    "delete",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "product_team_access_policy" {
+  key_vault_id = "${azurerm_key_vault.kv.id}"
+
+  object_id = "${var.product_group_object_id}"
+  tenant_id = "${var.tenant_id}"
+
+  key_permissions = [
+    "list",
+    "update",
+    "create",
+    "import",
+    "delete",
+  ]
+
+  certificate_permissions = [
+    "list",
+    "update",
+    "create",
+    "import",
+    "delete",
+    "managecontacts",
+    "manageissuers",
+    "getissuers",
+    "listissuers",
+    "setissuers",
+    "deleteissuers",
+  ]
+
+  secret_permissions = [
+    "list",
+    "set",
+    "delete",
+  ]
+}
