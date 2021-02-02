3f64424b94530c75e42c7c8b5cd3a5abffc2c564
Tim Jacomb <tim.jacomb@hmcts.net>
2021-02-02 13:36:05 +0000

    Give developers access to secrets in non production

---
+++ developer-access.tf
@@ -0,0 +1,30 @@
+data "azuread_group" "developers" {
+  name = var.developers_group
+}
+
+locals {
+  is_prod = length(regexall(".*(prod).*", var.env)) > 0
+}
+
+resource "azurerm_key_vault_access_policy" "developer" {
+  key_vault_id = azurerm_key_vault.kv.id
+  object_id    = data.azuread_group.developers.object_id
+  tenant_id    = var.tenant_id
+
+  key_permissions = [
+    "get",
+    "list",
+  ]
+
+  certificate_permissions = [
+    "get",
+    "list",
+  ]
+
+  secret_permissions = [
+    "get",
+    "list",
+  ]
+
+  count = local.is_prod ? 0 : 1
+}
