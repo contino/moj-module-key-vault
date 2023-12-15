2cb23a213becbb320215e83a338a26b96c69883c
Alex Bance <abance@bancey.xyz>
2023-12-15 09:49:47 +0000

    fix: added metrics block (#70)

--- diagnostic-settings.tf
+++ diagnostic-settings.tf
@@ -6,6 +6,12 @@ resource "azurerm_monitor_diagnostic_setting" "kv-ds" {
   enabled_log {
     category = "AuditEvent"
   }
+
+  lifecycle {
+    ignore_changes = [
+      metric,
+    ]
+  }
 }
 
 module "log_analytics_workspace" {
